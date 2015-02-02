118529b93cb4ea2a8da96aa54bf4903a03002c30
Eman Copty <eman.copty@intel.com>
2015-02-02 13:42:34 -0800

    Update depth formats in DepthImageFormat
    Adding UVMAP format
    Remove Y12 Format
    Clean Switch case from irrelevant standard formats
    add isPublicFormat function
    
    Change-Id: I79c99a92a3ce7b4b5f4b70b5640db4cfd5906011
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-26422
    Reviewed-on: https://android.intel.com/326837
    Reviewed-by: Greenberg, Aviv D <aviv.d.greenberg@intel.com>
    Tested-by: Greenberg, Aviv D <aviv.d.greenberg@intel.com>
    Reviewed-on: https://android.intel.com:443/345560

--- com/intel/camera2/extensions/depthcamera/DepthImageFormat.java
+++ com/intel/camera2/extensions/depthcamera/DepthImageFormat.java
@@ -1,47 +1,46 @@
-/*******************************************************************************
- * INTEL CORPORATION PROPRIETARY INFORMATION
- *
- * This software is supplied under the terms of a license agreement or nondisclosure
- * agreement with Intel Corporation and may not be copied or disclosed except in
- * accordance with the terms of that agreement
- * Copyright(c) 2014 Intel Corporation. All Rights Reserved.
- ********************************************************************************/
-
 package com.intel.camera2.extensions.depthcamera;
 
 import android.graphics.ImageFormat;
 
 public class DepthImageFormat extends ImageFormat {
-	
+
     /**
      * Depth Z format used for depth images, stored in 16 bit words. 
      */
     public static final int Z16 = 0x111;
-    
+
     /**
-     * Y12 is a YUV planar format comprised of a WxH Y plane only, with each pixel
-     * being represented by 12 bits.
-     */
-    public static final int Y12 = 0x1F1;
-    
+    * UVMap format 64bit each inlcudes 2 floats- size is resolution of Depth
+    */
+    public static final int UVMAP = 0x112;
+
     public static int getBitsPerPixel(int format) {
         switch (format) {
-            case RGB_565:
-            case NV16:
-            case YUY2:
-            case YV12:
-            case Y8:
-            case Y16:
-            case NV21:
-            case YUV_420_888:
-            case RAW_SENSOR:
-   //         case BAYER_RGGB:
-            	return ImageFormat.getBitsPerPixel(format);
-            case Y12:
-            	return 16;
+            case UVMAP:
+                return 64;
             case Z16:
-            	return 16;
+                return 16;
         }
-        return -1;
+        return ImageFormat.getBitsPerPixel(format);
+    }
+    /**
+     * Determine whether or not this is a public-visible {@code format}.
+     *
+     * <p>In particular, {@code @hide} formats will return {@code false}.</p>
+     *
+     * <p>Any other formats (including UNKNOWN) will return {@code false}.</p>
+     *
+     * @param format an integer format
+     * @return a boolean
+     *
+     * @hide
+     */
+    public static boolean isPublicFormat(int format) {
+        switch (format) {
+            case UVMAP:
+            case Z16:
+                return true;
+        }
+        return ImageFormat.isPublicFormat(format);
     }
 }
